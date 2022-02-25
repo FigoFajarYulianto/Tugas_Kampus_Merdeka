fd27721745638f14ec8d04fc0b0372d4f0d248dd
FigoFajarYulianto <71810352+FigoFajarYulianto@users.noreply.github.com>
2022-02-25 17:31:02 +0700

    Challenge

---
+++ Challange1_FigoFajarYulianto/assets_load/js/main.js
@@ -0,0 +1,23 @@
+/**
+* Template Name: HeroBiz - v2.0.0
+* Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  
+  
+
+});
